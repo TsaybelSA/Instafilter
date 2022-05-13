d4ebb999fbbe5f74944f4616ebdadaac78c15aae
TsaybelSA <tsaybelsa@gmail.com>
2022-05-13 10:44:11 +0600

    Prepared ImageSaver and ImagePicker struct.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -10,38 +10,11 @@ import SwiftUI
 
 
 struct ContentView: View {
-	@State private var image: Image?
-	@State private var inputImage: UIImage?
-	@State private var showImagePicker = false
 	
     var body: some View {
 		VStack {
-			image?
-				.resizable()
-				.scaledToFit()
 			
-			Button("Add image") {
-				showImagePicker = true
-			}
-			
-			Button("Save image") {
-				guard let inputImage = inputImage else { return }
-				let imageSaver = ImageSaver()
-				imageSaver.writeToPhotoAlbum(image: inputImage)
-			}
 		}
-		.sheet(isPresented: $showImagePicker) {
-			ImagePicker(image: $inputImage)
-		}
-		.onChange(of: inputImage) { _ in loadImage() }
-			
-	}
-	
-	func loadImage() {
-		guard let inputImage = inputImage else { return }
-		image = Image(uiImage: inputImage)
-		
-		UIImageWriteToSavedPhotosAlbum(inputImage, nil, nil, nil)
 	}
 }
 
